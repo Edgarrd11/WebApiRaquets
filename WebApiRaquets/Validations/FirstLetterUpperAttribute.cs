f9af8a66ebd255e96caffd183c2b03c69aaa8e60
Edgarrd11 <edgar.dorador@gmail.com>
2022-03-28 19:10:03 -0600

    Al tiro

---
+++ WebApiRaquets/Validations/FirstLetterUpperAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiRaquets.Validations
+{
+    public class FirstLetterUpperAttribute : ValidationAttribute //Permite implementar el metodo IsValid
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null || string.IsNullOrEmpty(value.ToString())) 
+            {
+                return ValidationResult.Success;
+            }
+
+            var primeraLetra = value.ToString()[0].ToString();
+            
+            if(primeraLetra != primeraLetra.ToUpper()) 
+            {
+                return new ValidationResult("First letter may upper");
+            }
+
+            return ValidationResult.Success;
+        }
+
+    }
+}
